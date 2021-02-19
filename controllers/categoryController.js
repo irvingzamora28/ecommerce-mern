4e21d6feaa88c7afd31b85cfb04627be837d9034
Irving Zamora <imzo15@hotmail.com>
2021-02-19 00:12:32 -0700

    Added Category model, controller and router

---
+++ controllers/categoryController.js
@@ -0,0 +1,8 @@
+const categoryController = {
+    getCategories: async(req, res) => {
+        res.json("Category controller")
+    }
+}
+
+
+module.exports = categoryController
---
+++ routes/categoryRouter.js
@@ -0,0 +1,5 @@
+const router = require("express").Router()
+const categoryController = require("../controllers/categoryController")
+router.route("/category").get(categoryController.getCategories)
+
+module.exports = router
