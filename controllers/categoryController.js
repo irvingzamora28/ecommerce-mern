e44eb98e3186dcbd44c03f488ede9d44bc030bc0
Irving Zamora <imzo15@hotmail.com>
2021-02-19 23:27:11 -0700

    CRUD for categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,48 @@
+const Category = require("../models/categoryModel")
+
 const categoryController = {
     getCategories: async(req, res) => {
-        res.json("Category controller")
+        try {
+            const categories = await Category.find()
+            res.json(categories)
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+
+    createCategory: async(req, res) => {
+        try {
+            const { name } = req.body;
+            const category = await Category.findOne({ name })
+            if (category) return res.status(400).json({ msg: "This category already exists." })
+
+            const newCategory = new Category({ name })
+
+            await newCategory.save()
+            res.json("create")
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+
+    deleteCategory: async(req, res) => {
+        try {
+            await Category.findByIdAndDelete(req.params.id);
+            res.json({ msg: "Deleted category" })
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+
+    updateCategory: async(req, res) => {
+        try {
+            const { name } = req.body
+            await Category.findOneAndUpdate({ _id: req.params.id }, { name })
+
+            res.json({ mgs: "Updated category" })
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
     }
 }
 
