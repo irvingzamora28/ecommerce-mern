8fdba030397e32764521fe94963f80308e3ba887
Irving Zamora <irvingzamora28@gmail.com>
2021-08-20 23:37:30 -0600

    Solved bug that didn't allow cart items to be displayed

--- client/src/components/mainpages/cart/Cart.js
+++ client/src/components/mainpages/cart/Cart.js
@@ -6,29 +6,33 @@ function Cart() {
     const state = useContext(GlobalState)
     const [cart] = state.userAPI.cart
     const [total, setTotal] = useState(0)
-
+    console.log("cart");
+    console.log(cart);
     if (cart.length === 0) {
-        return <h2 style={{textAlign: "center", fontSize: "5rem"}}>Cart Empty</h2>
+        return <h2 style={{ textAlign: "center", fontSize: "5rem" }}>Cart Empty</h2>
     }
     return (
         <div>
             {
                 cart.map((product) => {
-                    <div className="detail cart">
-                        <img src={product.images.url} alt={product.title} />
-                        <div className="box-detail">
-                            <h2>{product.title}</h2>
-                            <h3>${product.price * product.quantity}</h3>
-                            <p>{product.description}</p>
-                            <p>{product.content}</p>
-                            <div className="amount">
-                                <button> - </button>
-                                <span>{product.quantity}</span>
-                                <button> + </button>
+                    return (
+
+                        <div className="detail cart" key={product._id}>
+                            <img src={product.images.url} alt={product.title} />
+                            <div className="box-detail">
+                                <h2>{product.title}</h2>
+                                <h3>${product.price * product.quantity}</h3>
+                                <p>{product.description}</p>
+                                <p>{product.content}</p>
+                                <div className="amount">
+                                    <button> - </button>
+                                    <span>{product.quantity}</span>
+                                    <button> + </button>
+                                </div>
+                                <div className="delete">X</div>
                             </div>
-                            <div className="delete">X</div>
                         </div>
-                    </div>
+                    )
                 })
             }
 
